48ffd7952cf942848e6d716b00e448ae16c69b61
Alex Flick <xela.fli@gmail.com>
2025-02-24 17:28:19 +0000

    rm integer cast

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -1,34 +1,30 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
+{{ config(materialized="view") }}
 
-with tripdata as 
-(
-  select *,
-    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
-  from {{ source('staging','fhv_tripdata') }}
-  where dispatching_base_num is not null 
-)
+with
+    tripdata as (
+        select
+            *,
+            row_number() over (partition by dispatching_base_num, pickup_datetime) as rn
+        from {{ source("staging", "fhv_tripdata") }}
+        where dispatching_base_num is not null
+    )
 select
+    -- field description: https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_fhv.pdf
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
-    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("integer")) }} as dispatching_base_num,
-    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
-    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
-    
+    {{ dbt_utils.generate_surrogate_key(["dispatching_base_num", "pickup_datetime"]) }}
+    as tripid,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }}
+    as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }}
+    as dropoff_locationid,
+
     -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- renaming
-    cast(SR_Flag as numeric) as sr_flag,
-    Affiliated_base_number as affiliated_base_number
+    cast(sr_flag as numeric) as sr_flag,
+    affiliated_base_number as affiliated_base_number
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
