c589cb8349b7a4ac75dd7d2bd306818df5bfacd9
Alex Flick <xela.fli@gmail.com>
2025-02-24 19:30:04 +0000

    add from statement.

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -1,18 +1,14 @@
 {{ config(materialized="view") }}
 
-with
-    tripdata as (
-        select
-            *,
-            row_number() over (partition by dispatching_base_num, pickup_datetime) as rn
-        from {{ source("staging", "fhv_tripdata") }}
-        where dispatching_base_num is not null
-    )
+with tripdata as (
+    select *
+    from {{ source('staging', 'fhv_tripdata') }}  -- Ensure this points to the correct source
+)
+
 select
     -- field description: https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_fhv.pdf
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(["dispatching_base_num", "pickup_datetime"]) }}
-    as tripid,
+    -- {{ dbt_utils.generate_surrogate_key(["pickup_datetime"]) }} as tripid,
     {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }}
     as pickup_locationid,
     {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }}
@@ -20,11 +16,16 @@ select
 
     -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
 
     -- renaming
-    cast(sr_flag as numeric) as sr_flag,
-    affiliated_base_number as affiliated_base_number
+    cast(SR_Flag as numeric) as sr_flag,
+    Affiliated_base_number as affiliated_base_number
+from tripdata
 
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
-{% if var("is_test_run", default=true) %} limit 100 {% endif %}
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
